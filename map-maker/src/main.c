f68a73ea6b2c8c62cd3a7aeaeaa5ba8e5e2ce9f3
Eugen-str <eugen.01654@gmail.com>
2024-07-07 14:03:19 +0200

    not ready yet

--- map-maker/src/main.c
+++ map-maker/src/main.c
@@ -25,7 +25,7 @@ typedef struct{
     int length;
 }Logs;
 
-void logs_add(Logs *logs, char *str, int length){
+void logs_add(Logs *logs, const char *str, int length){
     strcpy(logs->logs, str);
     logs->started_time = GetTime();
     logs->length = length;
@@ -89,7 +89,7 @@ bool clicked_within_rect(Rectangle rect, Vector2 mouse_pos){
             rect.y + rect.height >= mouse_pos.y);
 }
 
-void update_buttons_up_down(Panel btn_up, Panel btn_down, Vector2 mouse_pos, int *n, Color highlight_color){
+void update_buttons_up_down(Panel btn_up, Panel btn_down, Vector2 mouse_pos, int *n, int max_y, Color highlight_color){
     if(clicked_within(btn_up, mouse_pos)){
         DrawRectangleLinesEx(btn_up.rect, 3, highlight_color);
         if(*n > 0 && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
@@ -99,7 +99,7 @@ void update_buttons_up_down(Panel btn_up, Panel btn_down, Vector2 mouse_pos, int
     }
     if(clicked_within(btn_down, mouse_pos)){
         DrawRectangleLinesEx(btn_down.rect, 3, highlight_color);
-        if(*n < 7 && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+        if(*n < max_y && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
             *n += 1;
         }
         return;
@@ -172,15 +172,22 @@ void draw_selected_tex(Panel selected_tex_view, Font font, Texture2D atlas, int 
     DrawTexturePro(atlas, source, selected_tex_view.rect, (Vector2){0, 0}, 0, WHITE);
 }
 
-void draw_game_panel(Panel game_panel, Texture2D atlas, int bg_buffer[16][16], bool show_bg, int fg_buffer[16][16], bool show_fg, Color line_color){
+void draw_game_panel(Panel game_panel, RenderTexture screen, Texture2D atlas, Camera2D camera,
+        int **bg_buffer, bool show_bg, int **fg_buffer, bool show_fg, int map_width, int map_height, Color line_color){
     int sq_size = game_panel.rect.width / 16;
-    for(int i = 1; i < 16; i++){
-        DrawLine(game_panel.rect.x, i * sq_size + game_panel.rect.y, sq_size + game_panel.rect.width, i * sq_size + game_panel.rect.y, line_color);
-        DrawLine(i * sq_size + game_panel.rect.x, game_panel.rect.y, i * sq_size + game_panel.rect.y, game_panel.rect.y + game_panel.rect.height, line_color);
+
+    BeginTextureMode(screen);
+    ClearBackground(game_panel.bg_color);
+    BeginMode2D(camera);
+    for(int i = 0; i < map_height; i++){
+        for(int j = 0; j < map_width; j++){
+            DrawRectangleLines(j * sq_size, i * sq_size,
+                    sq_size, sq_size, line_color);
+        }
     }
 
-    for(int i = 0; i < 16; i++){
-        for(int j = 0; j < 16; j++){
+    for(int i = 0; i < map_height; i++){
+        for(int j = 0; j < map_width; j++){
             int bg_tex = bg_buffer[j][i];
             int fg_tex = fg_buffer[j][i];
 
@@ -195,12 +202,15 @@ void draw_game_panel(Panel game_panel, Texture2D atlas, int bg_buffer[16][16], b
                 DrawTexturePro(atlas, fg_source, dest, (Vector2){0, 0}, 0, WHITE);
         }
     }
+    EndMode2D();
+    EndTextureMode();
 }
 
-void update_place_texture(Panel game_panel, Vector2 mouse_pos, int bg_buffer[16][16], int fg_buffer[16][16], int selected_tex, int current_layer){
+void update_place_texture(Panel game_panel, Camera2D camera, Vector2 mouse_pos, int **bg_buffer, int **fg_buffer, int selected_tex, int current_layer){
     int sq_size = game_panel.rect.width / 16;
     if(clicked_within(game_panel, mouse_pos) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-        int x = (mouse_pos.x - game_panel.rect.x) / sq_size, y = (mouse_pos.y - game_panel.rect.y) / sq_size;
+        int x = floor(mouse_pos.x - 2* game_panel.rect.x + camera.target.x) / sq_size,
+            y = floor(mouse_pos.y - 2* game_panel.rect.y + camera.target.y) / sq_size;
 
         if(current_layer == 0){ // background
             bg_buffer[x][y] = selected_tex;
@@ -218,28 +228,28 @@ void update_place_texture(Panel game_panel, Vector2 mouse_pos, int bg_buffer[16]
     }
 }
 
-void save_to_file(Vector2 spawn_point, int bg_buffer[16][16], int fg_buffer[16][16], int *objects, int object_count){
+void save_to_file(int map_width, int map_height, Vector2 spawn_point, int **bg_buffer, int **fg_buffer, int *objects, int object_count){
     FILE *map_file;
     map_file = fopen("saved_maps/map.txt", "w");
 
     // map width and height
-    fprintf(map_file, "16 16\n");
+    fprintf(map_file, "%d %d\n", map_width, map_height);
     // map background color
     fprintf(map_file, "0x3c3836\n");
     // spawn point
     fprintf(map_file, "%d %d\n", (int)spawn_point.x, (int)spawn_point.y);
     fprintf(map_file, "\n");
     // background tiles
-    for(int i = 0; i < 16; i++){
-        for(int j = 0; j < 16; j++){
+    for(int i = 0; i < map_height; i++){
+        for(int j = 0; j < map_width; j++){
             fprintf(map_file, "%d ", bg_buffer[j][i]);
         }
         fprintf(map_file, "\n");
     }
     // foreground tiles
     fprintf(map_file, "\n");
-    for(int i = 0; i < 16; i++){
-        for(int j = 0; j < 16; j++){
+    for(int i = 0; i < map_height; i++){
+        for(int j = 0; j < map_width; j++){
             fprintf(map_file, "%d ", fg_buffer[j][i]);
         }
         fprintf(map_file, "\n");
@@ -261,12 +271,13 @@ void save_to_file(Vector2 spawn_point, int bg_buffer[16][16], int fg_buffer[16][
 }
 
 
-void update_button_save(Panel button_save, Vector2 mouse_pos, Logs *logs, Color highlight_color, int bg_buffer[16][16], int fg_buffer[16][16], int *objects, int object_count){
+void update_button_save(Panel button_save, Vector2 mouse_pos, Logs *logs, Color highlight_color,
+        int map_width, int map_height, int **bg_buffer, int **fg_buffer, int *objects, int object_count){
     if(clicked_within(button_save, mouse_pos)){
         DrawRectangleLinesEx(button_save.rect, 3, highlight_color);
         if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
             DrawRectangleLinesEx(button_save.rect, 3, highlight_color);
-            save_to_file((Vector2){15, 0}, bg_buffer, fg_buffer, objects, object_count);
+            save_to_file(map_width, map_height, (Vector2){15, 0}, bg_buffer, fg_buffer, objects, object_count);
 
             logs_add(logs, "Saved map.txt to saved_maps directory! Don't forget to copy it to game folder", 5);
         }
@@ -329,7 +340,7 @@ void draw_objects(Panel game_panel, int *objects, int object_count, Color color)
             .width = objects[i*5 + 2] * sq_size,
             .height = objects[i*5 + 3] * sq_size,
         };
-        DrawRectangleRec(object, color);
+        DrawRectangleLinesEx(object, 16, color);
     }
 }
 void update_checkbox(Panel *checkbox_object, Vector2 mouse_pos, Color highlight_color, Color checked, Color unchecked, bool *check){
@@ -346,7 +357,7 @@ void update_checkbox(Panel *checkbox_object, Vector2 mouse_pos, Color highlight_
     }
 }
 
-void load_file(Logs *logs, int bg_buffer[16][16], int fg_buffer[16][16], int *objects, int *object_count, char *path){
+void load_file(Logs *logs, int **bg_buffer, int **fg_buffer, int *objects, int *object_count, char *path){
     FILE *map_file;
     map_file = fopen(path, "r");
     if(map_file == NULL){
@@ -397,8 +408,13 @@ void load_file(Logs *logs, int bg_buffer[16][16], int fg_buffer[16][16], int *ob
     logs_add(logs, TextFormat("Sucessfully loaded map file: %s", path), 5);
 }
 
+const int scrw = 1100, scrh = 1024;
+
 int main(int argc, char* argv[]){
-    InitWindow(1100, 1024, "sdfg map maker!");
+    InitWindow(scrw, scrh, "sdfg map maker!");
+
+    int map_width = 16;
+    int map_height = 16;
 
     Texture2D atlas = LoadTexture("../assets/tiles.png");
     Texture2D button_tex = LoadTexture("assets/button.png");
@@ -410,6 +426,7 @@ int main(int argc, char* argv[]){
     Color white = color_from_int(0xffffff);
     Color dusty_gray = color_from_int(0x665c54);
     Color green = color_from_int(0xb8bb26);
+    Color red = color_from_int(0xcc241d);
 
     Color bg_color = color_from_int(0x181818);
     Color panel_border_color = lblue;
@@ -464,6 +481,12 @@ int main(int argc, char* argv[]){
         .rect = (Rectangle){.x = button_select_fg.rect.x - checkbox_side - d,
             .y = button_select_fg.rect.y + (int)(checkbox_side / 2), .width = checkbox_side, .height = checkbox_side}};
 
+
+    int input_box_width = 200;
+    Panel filename_input_box = (Panel){.bg_color = game_bg_color, .border_color = lblue,
+        .rect = (Rectangle){.x = button_save.rect.x,
+            .y = button_select_fg.rect.y + (int)(checkbox_side / 2), .width = checkbox_side, .height = checkbox_side}};
+
     Panel *panels[] = {
         &game_panel,
         &tex_panel,
@@ -498,10 +521,14 @@ int main(int argc, char* argv[]){
     int *objects = malloc(sizeof(int) * 128);
     int object_count = 0;
 
-    int bg_buffer[16][16];
-    int fg_buffer[16][16];
-    for(int i = 0; i < 16; i++){
-        for(int j = 0; j < 16; j++){
+    int **bg_buffer = (int**)malloc(map_width* sizeof(int*));
+    int **fg_buffer = (int**)malloc(map_width* sizeof(int*));
+    for(int i = 0; i < map_height; i++){
+        bg_buffer[i] = (int*)malloc(map_height * sizeof(int));
+        fg_buffer[i] = (int*)malloc(map_height * sizeof(int));
+    }
+    for(int i = 0; i < map_width; i++){
+        for(int j = 0; j < map_height; j++){
             bg_buffer[j][i] = 0;
             fg_buffer[j][i] = 0;
         }
@@ -511,8 +538,19 @@ int main(int argc, char* argv[]){
         load_file(&logs, bg_buffer, fg_buffer, objects, &object_count, argv[1]);
     }
 
+    Camera2D camera = { 0 };
+    //camera.target = (Vector2){game_panel.rect.x + game_panel.rect.width/2, game_panel.rect.y + game_panel.rect.height/2};
+    //camera.offset = (Vector2){game_panel.rect.x + game_panel.rect.width/2, game_panel.rect.y + game_panel.rect.height/2};
+    camera.target = (Vector2){0, 0};
+    camera.offset = (Vector2){0, 0};
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
+
+    RenderTexture game_screen = LoadRenderTexture(game_panel.rect.width, game_panel.rect.height);
+    Rectangle screen_rect = { 0.0f, 0.0f, (float)game_screen.texture.width, (float)-game_screen.texture.height };
     SetTargetFPS(60);
     while(!WindowShouldClose()){
+        draw_game_panel(game_panel, game_screen, atlas, camera, bg_buffer, show_backround, fg_buffer, show_foreground, map_width, map_height, dusty_gray);
         BeginDrawing();
             ClearBackground(bg_color);
             draw_panels(panels, panel_count);
@@ -529,36 +567,58 @@ int main(int argc, char* argv[]){
 
             draw_selected_tex(selected_tex_view, iosevka, atlas, selected_tex, 16);
 
-            draw_game_panel(game_panel, atlas, bg_buffer, show_backround, fg_buffer, show_foreground, dusty_gray);
+            DrawTextureRec(game_screen.texture, screen_rect, (Vector2){.x = game_panel.rect.x, .y = game_panel.rect.y}, WHITE);
 
             draw_panel_text(button_save, iosevka, "SAVE", 48, WHITE);
 
+            //TODO: draw_object does not account for camera
             if(show_objects)
-                draw_objects(game_panel, objects, object_count, dblue);
+                draw_objects(game_panel, objects, object_count, red);
 
             if(IsMouseButtonDown(MOUSE_BUTTON_LEFT) || IsMouseButtonDown(MOUSE_BUTTON_RIGHT)){
                 Vector2 mouse_pos = GetMousePosition();
 
-                update_buttons_up_down(button_up, button_down, mouse_pos, &tex_panel_loc, dblue);
+                update_buttons_up_down(button_up, button_down, mouse_pos, &tex_panel_loc, atlas.height/16 -1, dblue);
                 update_buttons_select(&button_select_bg, &button_select_fg, &button_select_objects, mouse_pos,
                         &selected_layer, dblue, game_bg_color, selected_button);
-                update_button_save(button_save, mouse_pos, &logs, dblue, bg_buffer, fg_buffer, objects, object_count);
+                update_button_save(button_save, mouse_pos, &logs, dblue, map_width, map_height, bg_buffer, fg_buffer, objects, object_count);
 
                 update_texture_select(tex_panel, tex_panel_loc, &selected_tex, mouse_pos);
 
                 if(selected_layer == 2)
                     update_place_object(game_panel, mouse_pos, &object_start, &object_start_loc, objects, &object_count);
                 else
-                    update_place_texture(game_panel, mouse_pos, bg_buffer, fg_buffer, selected_tex, selected_layer);
+                    update_place_texture(game_panel, camera, mouse_pos, bg_buffer, fg_buffer, selected_tex, selected_layer);
 
                 update_checkbox(&checkbox_object, mouse_pos, dblue, green, game_bg_color, &show_objects);
                 update_checkbox(&checkbox_fg, mouse_pos, dblue, green, game_bg_color, &show_foreground);
                 update_checkbox(&checkbox_bg, mouse_pos, dblue, green, game_bg_color, &show_backround);
             }
 
+            if(IsKeyDown(KEY_LEFT) && camera.target.x > -5.0f * 48){
+                camera.target.x -= 5.0f;
+            }
+            if(IsKeyDown(KEY_RIGHT) && camera.target.x < map_width * 48 - 48 * 11){
+                camera.target.x += 5.0f;
+            }
+            if(IsKeyDown(KEY_UP) && camera.target.y > -5.0f * 48){
+                camera.target.y -= 5.0f;
+            }
+            if(IsKeyDown(KEY_DOWN) && camera.target.y < map_height * 48 - 48 * 11){
+                camera.target.y += 5.0f;
+            }
+
             logs_update_draw(&logs, iosevka, dblue);
         EndDrawing();
     }
+
+    for(int i = 0; i < map_height; i++){
+        free(bg_buffer[i]);
+        free(fg_buffer[i]);
+    }
+    free(bg_buffer);
+    free(fg_buffer);
+
     CloseWindow();
     return 0;
 }
